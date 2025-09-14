5747ab35bb20430ddc01505e6b6d0146f24ca65b
unknown <sacchanisbestgril@gmail.com>
2025-09-14 11:41:15 +0800

    style(frontend): styled sidebar

--- front-end/src/components/sidebar.jsx
+++ front-end/src/components/sidebar.jsx
@@ -10,13 +10,13 @@ function SidebarLayout({ children }) {
 
       <div className="drawer-side">
         <label htmlFor="sidebar" className="drawer-overlay"></label>
-        <ul className="menu p-4 w-64 min-h-full bg-base-200 text-base-content">
-          <li><a href="">Home</a></li>
-          <li><a href="student">Student</a></li>
-          <li><a href="program">Program</a></li>
-          <li><a href="college">College</a></li>
-          <li><a href="statistics">Statistics</a></li>
-          <li><a href="about">About</a></li>
+        <ul className="menu p-4 w-64 min-h-full bg-blue-300 text-base-content text-lg font-semibold space-y-2">
+          <li><a href="/"><i className="pi pi-home mr-2"></i> Home</a></li>
+          <li><a href="/student"><i className="pi pi-user mr-2"></i> Student</a></li>
+          <li><a href="/program"><i className="pi pi-book mr-2"></i> Program</a></li>
+          <li><a href="/college"><i className="pi pi-building mr-2"></i> College</a></li>
+          <li><a href="/statistics"><i className="pi pi-chart-bar mr-2"></i> Statistics</a></li>
+          <li><a href="/about"><i className="pi pi-info-circle mr-2"></i> About</a></li>
         </ul>
       </div>
     </div>
